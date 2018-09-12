232ae40ceb8270dbb4af74d343ddced44ad2b845
Diego ARCIS <arcisd@gmail.com>
2018-09-11 23:06:48 -0300

    Add files via upload

---
+++ 002_fiblast.py
@@ -0,0 +1,8 @@
+# Given an integer n, find the last digit of the nth Fibonacci number F n (that is, F n mod 10).
+
+def calc_fib_last(n):
+    F=[0,1]
+    for i in range(2,n+1): F.append((F[i-1]+F[i-2])%10)
+    return F[n]
+
+print(calc_fib_last(int(input())))
