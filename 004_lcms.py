dd19093a3faaca1a902624871f79a5b56dbf419d
Diego ARCIS <arcisd@gmail.com>
2018-09-11 23:25:11 -0300

    Add files via upload

---
+++ 004_lcms.py
@@ -0,0 +1,17 @@
+# Uses python3
+import sys
+
+def gcd(a,b):
+    if 0 in [a,b] or a==b:
+       return max([a,b])
+    else: 
+       return gcd(min([a,b]),max([a,b])%min([a,b]))
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    a, b = map(int, input.split())
+    if a==0 or b==0:
+       print(0)
+    else:
+       print(a*b//gcd(a,b))
+
